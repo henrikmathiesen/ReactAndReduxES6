934e1dd53394d63bcefd03137d71c484c88265cc
henrikmathiesen <henrik_mathiesen@hotmail.com>
2016-12-29 23:43:21 +0100

    -app is running

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+import routes from './routes';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import App from './components/App';
 
-ReactDOM.render(
-  <App />,
+render(
+  <Router history={hashHistory} routes={routes} />,
   document.getElementById('root')
 );
