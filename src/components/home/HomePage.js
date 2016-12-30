061afce06768cad3ea4c65cf9f0d7d18177935cf
henrikmathiesen <henrik_mathiesen@hotmail.com>
2016-12-30 15:05:49 +0100

    -bootstrap jumbotron widget

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-
-// TODO: import real jumbotron and use it
-// https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-bootstrap
-// https://react-bootstrap.github.io/components.html#jumbotron
+import { Jumbotron, Button } from 'react-bootstrap';
 
 class HomePage extends Component {
     render() {
         return (
-            <div className="jumbotron">
+            <Jumbotron>
                 <h1>Course Administration</h1>
                 <div>
-                    <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+                    <Link to="about">
+                        <Button bsStyle="primary" bsSize="large">Learn more</Button>
+                    </Link>
                 </div>
-            </div>
+            </Jumbotron>
         );
     }
 }
