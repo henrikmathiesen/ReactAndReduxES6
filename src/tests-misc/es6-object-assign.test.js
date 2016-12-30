6eea45aa94f6bca5152d8327218da735513b7bb5
henrikmathiesen <henrik_mathiesen@hotmail.com>
2016-12-30 22:10:36 +0100

    -testing object.assign

--- src/tests-misc/es6-object-assign.test.js
+++ src/tests-misc/es6-object-assign.test.js
@@ -1,17 +1,33 @@
 describe('The Object.assign() method', () => {
 
+    it('Should first get some understanding -- A', () => { 
+        const obj01 = { name: 'Adam' };
+        const obj02 = { name: 'Adam' };
+        const expected = obj01 === obj02;
+
+        expect(expected).toEqual(false);
+    });
+
+    it('Should first get some understanding -- B', () => { 
+        const obj01 = { name: 'Adam' };
+        const obj02 = obj01;
+        const expected = obj01 === obj02;
+
+        expect(expected).toEqual(true);
+    });
+
     it('Should work with babel and this test environment', () => {
-        var obj = { a: 1 };
-        var copy = Object.assign({}, obj);
+        const obj = { a: 1 };
+        const copy = Object.assign({}, obj);
 
         expect(obj).toEqual(copy);
         expect(obj).not.toBe(copy);
     });
 
     it('Should be able to change property on the cloned object', () => {
-        var obj = { name: 'Adam' };
-        var copy = Object.assign({}, obj, { name: 'Bertil' });
-        var expected = { name: 'Bertil' };
+        const obj = { name: 'Adam' };
+        const copy = Object.assign({}, obj, { name: 'Bertil' });
+        const expected = { name: 'Bertil' };
 
         expect(obj).not.toEqual(copy);
         expect(copy).toEqual(expected);
