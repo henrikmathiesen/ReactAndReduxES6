55250202a45e368df7ebdb55b9c6202fac9d6912
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-01 12:20:29 +0100

    -adding thunk as middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 import Environment from '../utils/Environment';
 
 export default function configureStore(initialState) {
-    if (Environment.getIsDebug()) {
-        return createStore(
-            rootReducer, initialState, applyMiddleware(reduxImmutableStateInvariant())
-        );
-    }
-    else {
-        return createStore(rootReducer, initialState);
-    }
+    const middleware = Environment.getIsDebug() ? [thunk, reduxImmutableStateInvariant()] : [thunk];
+    return createStore(rootReducer, initialState, applyMiddleware(...middleware));
 }
