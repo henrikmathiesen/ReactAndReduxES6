231eec5f84166a6b1205e674fefa9af74225a11e
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-01 12:55:34 +0100

    -load courses on page load

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
 import routes from './routes';
+import configureStore from './store/configureStore';
+import { loadCourses } from './actions/courseActions';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 const store = configureStore(); // send in state here from external sources, like local storage or from server
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
