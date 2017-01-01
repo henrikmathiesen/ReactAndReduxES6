2c4bda1871a49e6b7fefc00008ca2072dd4f9e49
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-01 15:56:42 +0100

    -only import redux-immutable-state-invariant in debug mode

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 import Environment from '../utils/Environment';
 
 export default function configureStore(initialState) {
-    const middleware = Environment.getIsDebug() ? [thunk, reduxImmutableStateInvariant()] : [thunk];
+    const middleware = Environment.getIsDebug() ? [thunk, require('redux-immutable-state-invariant')()] : [thunk];
     return createStore(rootReducer, initialState, applyMiddleware(...middleware));
 }
