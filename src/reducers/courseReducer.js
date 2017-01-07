61dab745b0026e601f5a3edc27005eed2f8863e7
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-07 16:14:00 +0100

    -comments

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,5 +1,7 @@
 import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from '../actions/courseActions';
 
+// Reducers should be pure functions, no side effects, do not call Math.random or new Date() in here
+
 export default function courseReducer(state = [], action) { 
     switch(action.type) {
         case CREATE_COURSE:
