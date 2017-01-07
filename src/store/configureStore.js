91ff0eb9441e2e9dfb689f63335081960d7fd66b
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-07 16:05:34 +0100

    -installed http-server as local package -configured dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 import Environment from '../utils/Environment';
 
 export default function configureStore(initialState) {
     const middleware = Environment.getIsDebug() ? [thunk, require('redux-immutable-state-invariant')()] : [thunk];
-    return createStore(rootReducer, initialState, applyMiddleware(...middleware));
+    const devTools = Environment.getIsDebug() && window.devToolsExtension ? window.devToolsExtension() : f => f;
+
+    return createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware),
+            devTools
+        )
+    );
 }
