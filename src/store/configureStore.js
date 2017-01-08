3ef68a39ec2aed8ebe2de7c2049549920b1be656
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-08 11:01:55 +0100

    -comments

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,6 +3,8 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 import Environment from '../utils/Environment';
 
+// In Redux there is only one single store
+
 export default function configureStore(initialState) {
     const middleware = Environment.getIsDebug() ? [thunk, require('redux-immutable-state-invariant')()] : [thunk];
     const devTools = Environment.getIsDebug() && window.devToolsExtension ? window.devToolsExtension() : f => f;
