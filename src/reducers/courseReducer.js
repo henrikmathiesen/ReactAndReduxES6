37bcd041c44fd9bf4c68eaf3c186e02c8054ba10
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-01-08 10:53:37 +0100

    -comments

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -5,6 +5,8 @@ import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from '../actions/courseActions';
 export default function courseReducer(state = [], action) { 
     switch(action.type) {
         case CREATE_COURSE:
+            // copy the old state array (spread it out), create a new object from action.course and add it to the array
+            // This way we are not mutating state
             return [...state,
                 Object.assign({}, action.course)
             ];
